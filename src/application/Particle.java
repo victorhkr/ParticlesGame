28c18739c659bdd1b4cc86f666b02a6f80bcb65e
Victor <victor.hkr@hotmail.com>
2025-06-22 14:42:34 -0300

    Melhorias Gerais

--- src/application/Particle.java
+++ src/application/Particle.java
@@ -4,90 +4,57 @@ import javafx.scene.paint.Color;
 import javafx.scene.shape.Circle;
 
 public class Particle {
-	public Circle particleCircle;
-	public double posX;
-	public double posY;
-	public double velocityX=0;
-	public double velocityY=0;
-	public double accelX=0;
-	public double accelY=0;
-	public double forceX=0;
-	public double forceY=0;
-	public double kinectEnergy=0;
-	public double mass=1000;
-	public double circleSize;
-	private static double timeStep = 0.01;
+    private double posX, posY;
+    private double velocityX, velocityY;
+    private double forceX, forceY;
+    private double mass;
+    private double circleSize;
+    private Circle particleCircle;
+    private static double timeStep = 0.01;
 
+    public Particle(double size, double x, double y) {
+        this.circleSize = size;
+        this.posX = x;
+        this.posY = y;
+        this.mass = Math.PI * size * size;
+        this.particleCircle = new Circle(x, y, size, Color.BLACK);
+    }
 
-	public Particle(int circleSize, double x, double y) {
-		posX = x;
-		posY = y;
-		this.circleSize = circleSize;
-		mass = 3.14159265359*circleSize*circleSize;
-		particleCircle = new Circle(x, y, circleSize, Color.BLACK);
-	}
-	public Particle(int circleSize,double x, double y,double vx,double vy) {
-		posX = x;
-		posY = y;
-		velocityX=vx;
-		velocityY=vy;
-		this.circleSize = circleSize;
-		mass = 3.14159265359*circleSize*circleSize;
-		particleCircle = new Circle(x, y, circleSize, Color.BLACK);
-	}
-	public Particle(int circleSize,double x, double y,double vx,double vy,double ax,double ay) {
-		posX = x;
-		posY = y;
-		velocityX=vx;
-		velocityY=vy;
-		accelX=ax;
-		accelY=ay;
-		this.circleSize = circleSize;
-		mass = 3.14159265359*circleSize*circleSize;
-		particleCircle = new Circle(x, y, circleSize, Color.BLACK);
-	}
-		
-	public void applyForce(double forceX, double forceY) {
-	    double accelerationX = forceX / mass;
-	    double accelerationY = forceY / mass;
-	    velocityX += accelerationX * timeStep;
-	    velocityY += accelerationY * timeStep;
-	    posX += velocityX * timeStep;
-	    posY += velocityY * timeStep;
-		//System.out.println(velocityX);
-		//System.out.println(velocityY);
+    public void resetForces() {
+        forceX = 0;
+        forceY = 0;
+    }
 
-	}
-	
-	public void applyForce2() {
-	    accelX = forceX / mass;
-	    accelY = forceY / mass;
-	    velocityX += accelX * timeStep;
-	    velocityY += accelY * timeStep;
-	    posX += velocityX * timeStep;
-	    posY += velocityY * timeStep;
-		//System.out.println(Math.sqrt(forceX*forceX+forceY*forceY));
-		//System.out.println(velocityY);
+    public void addForce(double fx, double fy) {
+        forceX += fx;
+        forceY += fy;
+    }
 
-	}
-	
-	public double particleEnergy() {
-		kinectEnergy = mass*(velocityX*velocityX+velocityY*velocityY)/2;
-		
-		//System.out.println(kinectEnergy);
-		return kinectEnergy;
-	}
-	public double particleEnergyX() {
-		kinectEnergy = mass*(velocityX*velocityX)/2;
-		
-		//System.out.println(kinectEnergy);
-		return kinectEnergy;
-	}
-	public double particleEnergyY() {
-		kinectEnergy = mass*(velocityY*velocityY)/2;
-		
-		//System.out.println(kinectEnergy);
-		return kinectEnergy;
-	}
-	
+    public void updatePhysics() {
+        double accelX = forceX / mass;
+        double accelY = forceY / mass;
+        
+        velocityX += accelX * timeStep;
+        velocityY += accelY * timeStep;
+        
+        posX += velocityX * timeStep;
+        posY += velocityY * timeStep;
+    }
+
+    public void updateView() {
+        particleCircle.setCenterX(posX);
+        particleCircle.setCenterY(posY);
+    }
+
+    // Getters e Setters
+    public double getPosX() { return posX; }
+    public double getPosY() { return posY; }
+    public void setPos(double x, double y) { posX = x; posY = y; }
+    public double getVelocityX() { return velocityX; }
+    public double getVelocityY() { return velocityY; }
+    public void setVelocity(double vx, double vy) { velocityX = vx; velocityY = vy; }
+    public double getCircleSize() { return circleSize; }
+    public double getMass() { return mass; }
+    public void setMass(double mass) { this.mass = mass; }
+    public Circle getParticleCircle() { return particleCircle; }
 }
--- src/module-info.java
+++ src/module-info.java
@@ -1,5 +1,7 @@
 module ParticlesGame {
 	requires javafx.controls;
+	requires javafx.base;
+	requires javafx.graphics;
 	
 	opens application to javafx.graphics, javafx.fxml;
 }
